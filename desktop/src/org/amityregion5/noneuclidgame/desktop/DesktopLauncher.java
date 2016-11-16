9f4c600ef3dbc482da51f3cbed6c197f03f99971
SergeySave <sergeysav.nn@gmail.com>
2016-11-16 15:11:15 -0500

    adds LibGDX stuffs

---
+++ desktop/src/org/amityregion5/noneuclidgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.amityregion5.noneuclidgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.amityregion5.noneuclidgame.NonEuclidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NonEuclidGame(), config);
+	}
+}
