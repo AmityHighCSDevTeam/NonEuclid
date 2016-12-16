f8b46127c29c46d06ecd13b3054e1240f58f9228
TimWF8 <TimWF8@gmail.com>
2016-12-16 09:03:15 -0500

    Started Program

--- desktop/src/org/amityregion5/noneuclidgame/desktop/DesktopLauncher.java
+++ desktop/src/org/amityregion5/noneuclidgame/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import org.amityregion5.noneuclidgame.NonEuclidGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 300;
+		config.height = 300;
 		new LwjglApplication(new NonEuclidGame(), config);
 	}
 }
